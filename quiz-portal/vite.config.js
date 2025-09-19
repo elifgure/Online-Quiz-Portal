33671292e33dfb998049f05a69ce5a54a3820993
elifgure <elif.gure121@gmail.com>
2025-09-20 01:47:20 +0300

    feat: Add Vercel configuration file and update vite.config.js for build settings

--- quiz-portal/vite.config.js
+++ quiz-portal/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',     // Vercel’in deploy edeceği klasör
+  },
+  base: './',            // Eğer Vercel'de alt route'lar varsa, bu önemli
 })
